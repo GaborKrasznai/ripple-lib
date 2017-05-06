e8351b6de6f6538dc076b74ad12739993e9098b8
GaborKrasznai <kraszi2016@gmail.com>
2017-05-06 23:48:08 +0100

    Update balances.js

--- docs/samples/balances.js
+++ docs/samples/balances.js
@@ -1,5 +1,5 @@
 'use strict';
-const RippleAPI = require('../../src').RippleAPI; // require('ripple-lib')
+const RippleAPI = require('ripple-lib').RippleAPI; // require('ripple-lib')
 
 const api = new RippleAPI({server: 'wss://s1.ripple.com:443'});
 const address = 'r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV';
